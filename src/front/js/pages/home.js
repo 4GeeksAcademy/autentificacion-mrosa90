39fc26e226c0796fb907286100561184ab128b44
Mrosa90 <145835544+Mrosa90@users.noreply.github.com>
2024-06-05 16:50:41 +0000

    terminado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,25 +2,25 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <h1>HOME</h1>
+      
+        <div style={{display:'flex', justifyContent: 'space-around', width:'300px' }} className="container">
+          <Link  to="/login">
+            <button>Go to login</button>
+          </Link>
+          <br />
+          <Link to="/register">
+            <button>Go to register</button>
+          </Link>
+        </div>
+      
+    </div>
+  );
 };
